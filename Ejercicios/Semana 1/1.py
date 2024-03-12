bd0b009182da4187bbfc69f3ac29304787d0abd2
VICTOR RUIZ <veruiz@uc.cl>
2024-03-12 12:50:09 -0300

    xd

---
+++ Ejercicios/Semana 1/1.py
@@ -0,0 +1,28 @@
+class Alumno:
+    
+    def __init__(self, nombre, apellido, numero_alumno):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.numero_alumno = numero_alumno
+        self.clases_asistidas = []
+    
+class Sala:
+    
+    def __init__(self, lista_alumnos, numero_sala, sigla):
+        self.lista_alumnos = lista_alumnos
+        self.numero_sala = numero_sala
+        self.sigla = sigla
+
+    
+    def ingresar_alumno(self, alumno):
+        if alumno.nombre in
+if __name__ == "__main__":
+    javier = Alumno("Javier", "Gomez", "18639942")
+    josefa = Alumno("Josefa", "Paz", "16645578")
+    tomas = Alumno("Tomas", "Gonzalez", "19638225")
+
+    sala = Sala([("Michael", "19643721"),("Silvana", "18632890"),("Tomas", "19638225"),
+                 ("Josefa", "16645578"),("Javier", "16665798"),("", "")], 666, "IIC2233")
+    sala.ingresar_alumno(javier)
+    sala.ingresar_alumno(josefa)
+    sala.ingresar_alumno(tomas)
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Apuntes-Avanzada-2024-1 xd
 
 ## [Clase 1](./Clases/Clase%201.md)
+### Ejericios clase 1 [Ejercicios](./Ejercicios/Semana%201/1.py)
+
