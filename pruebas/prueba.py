b002ca427face6ad6e536ca4883fc487ef4429a5
VICTOR RUIZ <veruiz@uc.cl>
2024-03-14 15:37:00 -0300

    xd

---
+++ pruebas/prueba.py
@@ -0,0 +1,8 @@
+#Cree este archivo con comandos de terminal
+#Para probar el funcionamiento de git
+#Y para probar el funcionamiento de git en visual studio code
+#Y para probar el funcionamiento de git en visual studio code en windows
+
+
+if __name__ == "__main__":
+    print("xd")
