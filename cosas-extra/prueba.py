3448422117d90408f282906358efcb8c6ff035a8
VICTOR RUIZ <veruiz@uc.cl>
2024-03-19 09:42:39 -0300

    actualización

--- cosas-extra/prueba.py
+++ cosas-extra/prueba.py
@@ -1,9 +1,15 @@
 
-
-
-archivo = open("correos.csv", "r")
+archivo = open("texto.txt", "r")
 data = archivo.readlines()
-data = [line.rstrip('\n').split('\t') for line in data]
-correos = [line[2] for line in data]
-for correo in correos:
-    print(correo)
+info = [line.rstrip('\n') for line in data]
+print(info)
+numeros = info[-1].split(",")
+print(numeros)
+lista_red = []
+cantidad_de_estaciones = int(info[0])
+for contador in range(cantidad_de_estaciones):
+    lista_red.append(numeros[contador: contador + cantidad_de_estaciones])
+for contador in range(len(lista_red)):
+    for mini_contador in range(len(lista_red[contador])):
+        lista_red[contador][mini_contador] = int(lista_red[contador][mini_contador])
+print(lista_red)
