a9a3da6c3608d3a77359bf439fd8b9e22b55dcea
VICTOR RUIZ <veruiz@uc.cl>
2024-03-19 02:52:25 -0300

    xd

--- cosas-extra/prueba.py
+++ cosas-extra/prueba.py
@@ -1,10 +1,9 @@
-#Cree este archivo con comandos de terminal
-#Para probar el funcionamiento de git
-#Y para probar el funcionamiento de git en visual studio code
-#Y para probar el funcionamiento de git en visual studio code en windows
 
 
-if __name__ == "__main__":
-    lista = [[0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0], [1, 1, 1, 0]]
-    print([sum(sublista) for sublista in lista])
-    
+
+archivo = open("correos.csv", "r")
+data = archivo.readlines()
+data = [line.rstrip('\n').split('\t') for line in data]
+correos = [line[2] for line in data]
+for correo in correos:
+    print(correo)
