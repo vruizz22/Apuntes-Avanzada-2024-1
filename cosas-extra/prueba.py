7733eca5839ce8f97f6fc6dbbb38379887953e65
VICTOR RUIZ <veruiz@uc.cl>
2024-04-14 14:55:41 -0400

    Ayudantia initial commit

--- cosas-extra/prueba.py
+++ cosas-extra/prueba.py
@@ -1,8 +1,27 @@
-def count_up_to(n):
-    count = 0
-    while count <= n:
-        yield count
-        count += 1
+import collections
 
-for number in count_up_to(4):
-    print(number)
+class Banco:
+    def __init__(self, dinero):
+        self.dinero_setter(dinero)
+        self.historial = collections.deque()
+        
+    @property
+    def dinero(self):
+        self.historial.appendleft(self._dinero_guardado)
+        self.dinero = 2
+        return self.historial[-1]
+    
+    @dinero.setter
+    def dinero(self, dinero):
+        print("Entre :)")
+        self._dinero_guardado += dinero
+        self.historial.append(self._dinero_guardado)
+        
+    def dinero_setter(self, dinero):
+        self._dinero_guardado = dinero
+        
+    
+banco = Banco(1)
+banco.dinero = banco.dinero + 2
+dinero = banco.dinero
+print(banco.historial)
